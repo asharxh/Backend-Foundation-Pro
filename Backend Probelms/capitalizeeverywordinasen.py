bc9081d75e39ca968bb0e93de2338ae73c7cfb2f
Ashar Arif <58476697+asharxh@users.noreply.github.com>
2025-10-08 19:22:55 +0530

    Add files via upload

---
+++ Backend Probelms/capitalizeeverywordinasen.py
@@ -0,0 +1,15 @@
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+capitalized_words = []
+for word in words:
+    if len(word) > 0:
+        capitalized_word = word[0].upper() + word[1:]
+        capitalized_words.append(capitalized_word)
+    else:
+        capitalized_words.append(word)
+
+capitalized_sentence = " ".join(capitalized_words)
+
+print("Capitalized sentence:", capitalized_sentence)
