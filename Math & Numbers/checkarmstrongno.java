b40f66c63aab12675266520b88bf0587230e54b6
Ashar Arif <58476697+asharxh@users.noreply.github.com>
2025-10-24 23:21:47 +0530

    Check Armstrong Number

---
+++ Math & Numbers/checkarmstrongno.java
@@ -0,0 +1,12 @@
+int num = 153;
+int original = num;
+int result = 0;
+int digits = String.valueOf(num).length();
+
+while (num != 0) {
+  int digit = num % 10;
+  result += Math.pow(digit, digits);
+  num /= 10;
+}
+
+System.out.println(original + (result == original ? " is Armstrong" : " is not Armstrong"));
